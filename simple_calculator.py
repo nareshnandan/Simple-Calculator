a0b841ee3a19e930081b3c74c73bb085b8ba813e
nareshnandan <nareshnandan23@gmail.com>
2024-11-12 13:37:01 +0530

    Simple Calculator

---
+++ simple_calculator.py
@@ -0,0 +1,26 @@
+def calculator():
+    
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    # Perform the calculation based on the operator
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error! Division by zero."
+    else:
+        result = "Invalid operator!"
+
+    print(f"Result: {result}")
+
+# Run the calculator
+calculator()
+
